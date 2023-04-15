aa7f4ff3435e6080070e44b18198b3a9923c76a3
ngthvan1612@gmail.com <ngthvan1612@gmail.com>
2023-04-15 18:37:39 +0700

    Create HomeActivity

---
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.hcmute.finalproject.toeicapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hcmute.finalproject.toeicapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
