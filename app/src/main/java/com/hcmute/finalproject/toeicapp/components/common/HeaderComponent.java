d63439b764734620b99b3a33907f80b453acd594
qhuong1008 <qhuong1008@gmail.com>
2023-04-16 22:02:06 +0700

    header

--- app/src/main/java/com/hcmute/finalproject/toeicapp/components/common/HeaderComponent.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/components/common/HeaderComponent.java
@@ -6,26 +6,27 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.hcmute.finalproject.toeicapp.R;
 
-public class BackButtonRoundedComponent extends LinearLayout {
-
-    public BackButtonRoundedComponent(Context context) {
+public class HeaderComponent extends LinearLayout {
+    public HeaderComponent(Context context) {
         this(context, null);
     }
 
-    public BackButtonRoundedComponent(Context context, @Nullable AttributeSet attrs) {
+    public HeaderComponent(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public BackButtonRoundedComponent(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public HeaderComponent(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.initComponent(context, attrs, defStyleAttr);
     }
 
+    public HeaderComponent(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
     private void initComponent(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        View view = inflate(context, R.layout.component_back_button_rounded, this);
+        View view = inflate(context, R.layout.component_common_header, this);
     }
 }
