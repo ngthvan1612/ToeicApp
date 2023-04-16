e834b8c24fa882f68c45a93b9e78ac775d32a7ec
ngthvan1612@gmail.com <ngthvan1612@gmail.com>
2023-04-17 02:44:15 +0700

    FIX & FEAT: fix part 1 component + create test part 1

--- app/src/main/java/com/hcmute/finalproject/toeicapp/components/part_one/PartOnePhotographsComponent.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/components/part_one/PartOnePhotographsComponent.java
@@ -15,8 +15,7 @@ public class PartOnePhotographsComponent extends LinearLayout {
     }
 
     public PartOnePhotographsComponent(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs,0);
-
+        this(context, attrs, 0);
     }
 
     public PartOnePhotographsComponent(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
