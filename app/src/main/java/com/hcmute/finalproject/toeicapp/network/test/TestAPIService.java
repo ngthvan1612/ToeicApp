65ae41310d6f80a1f6b1e782342badd41655c3e7
ngthvan1612@gmail.com <ngthvan1612@gmail.com>
2023-04-17 10:08:04 +0700

    UPDATE: add 600 toeic url retrofit2 (test)

--- app/src/main/java/com/hcmute/finalproject/toeicapp/network/test/TestAPIService.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/network/test/TestAPIService.java
@@ -10,4 +10,8 @@ public interface TestAPIService {
     @Streaming
     @GET("https://dl.dropboxusercontent.com/s/kn9qtfdiaqg2gxo/ahihi-nguyen-thanh-van-20110224.mp3")
     Call<ResponseBody> downloadTestFile01();
+
+    @Streaming
+    @GET("https://dl.dropboxusercontent.com/s/xin6vvva0shii3n/600-tu-toeic.json")
+    Call<ResponseBody> download600ToeicVocabularies();
 }
