da4817fb3800a664acdc1c7630a4005169999556
ngthvan1612@gmail.com <ngthvan1612@gmail.com>
2023-04-17 11:53:24 +0700

    UPDATE: create dao inside AppDatabase

--- app/src/main/java/com/hcmute/finalproject/toeicapp/database/ToeicAppDatabase.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/database/ToeicAppDatabase.java
@@ -3,6 +3,10 @@ package com.hcmute.finalproject.toeicapp.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.hcmute.finalproject.toeicapp.dao.ToeicDao;
+import com.hcmute.finalproject.toeicapp.dao.ToeicStorageDao;
+import com.hcmute.finalproject.toeicapp.dao.ToeicVocabularyDao;
+import com.hcmute.finalproject.toeicapp.dao.ToeicVocabularyTopicDao;
 import com.hcmute.finalproject.toeicapp.entities.ToeicStorage;
 import com.hcmute.finalproject.toeicapp.entities.ToeicVocabulary;
 import com.hcmute.finalproject.toeicapp.entities.ToeicVocabularyTopic;
@@ -16,5 +20,7 @@ import com.hcmute.finalproject.toeicapp.entities.ToeicVocabularyTopic;
         version = 1
 )
 public abstract class ToeicAppDatabase extends RoomDatabase {
-
+        public abstract ToeicStorageDao getToeicStorageDao();
+        public abstract ToeicVocabularyDao getToeicVocabularyDao();
+        public abstract ToeicVocabularyTopicDao getToeicVocabularyTopicDao();
 }
