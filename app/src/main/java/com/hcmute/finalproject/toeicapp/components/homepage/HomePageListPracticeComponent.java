c578d5801bde7e566aca62819ff2da1c5da919cb
baongoc02 <tranthibaongoc779152@gmail.com>
2023-04-17 21:17:29 +0700

    đổ bóng cho các component trong component_home_page_list_practice.xml

--- app/src/main/java/com/hcmute/finalproject/toeicapp/components/homepage/HomePageListPracticeComponent.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/components/homepage/HomePageListPracticeComponent.java
@@ -2,6 +2,9 @@ package com.hcmute.finalproject.toeicapp.components.homepage;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +17,7 @@ import androidx.annotation.Nullable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.hcmute.finalproject.toeicapp.R;
+import com.hcmute.finalproject.toeicapp.components.HomeButtonRoundedComponent;
 
 public class HomePageListPracticeComponent extends LinearLayout {
     public HomePageListPracticeComponent(Context context) {
@@ -31,5 +35,12 @@ public class HomePageListPracticeComponent extends LinearLayout {
 
     private void initComponent(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         View view = inflate(context, R.layout.component_home_page_list_practice, this);
+
+        //HomeButtonRoundedComponent btnPart1 = view.findViewById(R.id.component_home_page_list_practice_btn_part_1);
+        //HomeButtonRoundedComponent btnPart2 = view.findViewById(R.id.component_home_page_list_practice_btn_part_2);
+
+        if (this.isInEditMode()) {
+            return;
+        }
     }
 }
---
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/testing/ngoc/activities/NgocTestBlurShadowActivity.java
@@ -0,0 +1,16 @@
+package com.hcmute.finalproject.toeicapp.testing.ngoc.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hcmute.finalproject.toeicapp.R;
+
+public class NgocTestBlurShadowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngoc_test_blur_shadow);
+    }
+}
