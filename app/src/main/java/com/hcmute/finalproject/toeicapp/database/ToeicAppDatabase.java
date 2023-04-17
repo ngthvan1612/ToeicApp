f5d7fed49d5d7ae216e5ef1a81c03061e07df482
ngthvan1612@gmail.com <ngthvan1612@gmail.com>
2023-04-17 11:56:54 +0700

    FEAT: Create Appdatabase Instance

--- app/src/main/java/com/hcmute/finalproject/toeicapp/database/ToeicAppDatabase.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/database/ToeicAppDatabase.java
@@ -1,9 +1,11 @@
 package com.hcmute.finalproject.toeicapp.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import com.hcmute.finalproject.toeicapp.dao.ToeicDao;
 import com.hcmute.finalproject.toeicapp.dao.ToeicStorageDao;
 import com.hcmute.finalproject.toeicapp.dao.ToeicVocabularyDao;
 import com.hcmute.finalproject.toeicapp.dao.ToeicVocabularyTopicDao;
@@ -11,16 +13,23 @@ import com.hcmute.finalproject.toeicapp.entities.ToeicStorage;
 import com.hcmute.finalproject.toeicapp.entities.ToeicVocabulary;
 import com.hcmute.finalproject.toeicapp.entities.ToeicVocabularyTopic;
 
-@Database(
-        entities = {
-                ToeicStorage.class,
-                ToeicVocabulary.class,
-                ToeicVocabularyTopic.class
-        },
-        version = 1
-)
+@Database(entities = {ToeicStorage.class, ToeicVocabulary.class, ToeicVocabularyTopic.class}, version = 1)
 public abstract class ToeicAppDatabase extends RoomDatabase {
-        public abstract ToeicStorageDao getToeicStorageDao();
-        public abstract ToeicVocabularyDao getToeicVocabularyDao();
-        public abstract ToeicVocabularyTopicDao getToeicVocabularyTopicDao();
+    private final static String DATABASE_FILE_NAME = "toeic-db";
+
+    public abstract ToeicStorageDao getToeicStorageDao();
+
+    public abstract ToeicVocabularyDao getToeicVocabularyDao();
+
+    public abstract ToeicVocabularyTopicDao getToeicVocabularyTopicDao();
+
+    public static ToeicAppDatabase instance;
+
+    public static ToeicAppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), ToeicAppDatabase.class, DATABASE_FILE_NAME).allowMainThreadQueries().build();
+        }
+
+        return instance;
+    }
 }
