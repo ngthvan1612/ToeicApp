07203f5bfc17a2d81e481e1a1eba3b03e4248db3
ngthvan1612@gmail.com <ngthvan1612@gmail.com>
2023-04-17 11:57:10 +0700

    FEAT: Create download file service

---
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/services/storage/DownloadFileService.java
@@ -0,0 +1,12 @@
+package com.hcmute.finalproject.toeicapp.services.storage;
+
+import android.content.Context;
+
+public class DownloadFileService {
+    private final Context context;
+    
+
+    public DownloadFileService(Context context) {
+        this.context = context;
+    }
+}
