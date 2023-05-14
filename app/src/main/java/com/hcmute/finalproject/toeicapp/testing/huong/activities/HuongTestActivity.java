489e68b4c8f22e9485c809fa83c64b45c3c16cba
qhuong1008 <qhuong1008@gmail.com>
2023-05-14 14:18:05 +0700

    fix dialog in test package

--- app/src/main/java/com/hcmute/finalproject/toeicapp/testing/huong/activities/HuongTestActivity.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/testing/huong/activities/HuongTestActivity.java
@@ -13,12 +13,13 @@ import com.hcmute.finalproject.toeicapp.components.LoadingDialogComponent;
 
 public class HuongTestActivity extends AppCompatActivity {
     private Button btn;
-    Dialog dialog = new Dialog(HuongTestActivity.this,android.R.style.Theme_Light);
+    Dialog dialog;
 
     private LoadingDialogComponent loadingDialogComponent = new LoadingDialogComponent(HuongTestActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dialog = new Dialog(this,android.R.style.Theme_Light);
         setContentView(R.layout.activity_huong_test);
         btn = (Button) findViewById(R.id.loading_demo_button);
 
