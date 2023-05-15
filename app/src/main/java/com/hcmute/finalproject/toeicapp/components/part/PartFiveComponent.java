349d8866f5126fefdeec2d4f96e1cf5773690526
qhuong1008 <qhuong1008@gmail.com>
2023-05-15 20:38:37 +0700

    part five fix layout error

--- app/src/main/java/com/hcmute/finalproject/toeicapp/components/part/PartFiveComponent.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/components/part/PartFiveComponent.java
@@ -8,11 +8,23 @@ import android.widget.LinearLayout;
 import androidx.annotation.Nullable;
 
 import com.hcmute.finalproject.toeicapp.R;
+import com.hcmute.finalproject.toeicapp.components.AnswerSelectionComponent;
 import com.hcmute.finalproject.toeicapp.components.common.CommonHeaderComponent;
+import com.hcmute.finalproject.toeicapp.dao.ToeicAnswerChoiceDao;
+import com.hcmute.finalproject.toeicapp.dao.ToeicItemContentDao;
+import com.hcmute.finalproject.toeicapp.dao.ToeicQuestionDao;
+import com.hcmute.finalproject.toeicapp.database.ToeicAppDatabase;
+import com.hcmute.finalproject.toeicapp.entities.ToeicAnswerChoice;
+import com.hcmute.finalproject.toeicapp.entities.ToeicItemContent;
+import com.hcmute.finalproject.toeicapp.entities.ToeicQuestion;
 import com.hcmute.finalproject.toeicapp.entities.ToeicQuestionGroup;
+
+import java.util.List;
 
 public class PartFiveComponent extends ToeicPartComponentBase {
     private CommonHeaderComponent commonHeaderComponent;
+    public AnswerSelectionComponent answerSelectionComponent;
+
     public PartFiveComponent(Context context) {
         this(context, null);
     }
@@ -29,18 +41,40 @@ public class PartFiveComponent extends ToeicPartComponentBase {
     }
     private void initComponent(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         View view = inflate(context, R.layout.component_part_five, this);
-        commonHeaderComponent=view.findViewById(R.id.common_part_five_header_title);
-        commonHeaderComponent.setTitle("Part two - Questions and response");
+        this.answerSelectionComponent = view.findViewById(R.id.component_part_five_answer_selection);
     }
 
     @Override
     public void loadQuestionGroup(ToeicQuestionGroup toeicQuestionGroup) {
+        ToeicAppDatabase toeicAppDatabase = ToeicAppDatabase.getInstance(getContext());
+        ToeicQuestionDao toeicQuestionDao = toeicAppDatabase.getToeicQuestionDao();
+        ToeicAnswerChoiceDao toeicAnswerChoiceDao = toeicAppDatabase.getToeicAnswerChoiceDao();
+        ToeicQuestion toeicQuestion = toeicQuestionDao.getToeicQuestionByQuestionGroppId(toeicQuestionGroup.getId()).get(0);
+        List<ToeicAnswerChoice> choices = toeicAnswerChoiceDao.getByQuestionId(toeicQuestion.getId());
 
+        this.answerSelectionComponent.setQuestionTitle(toeicQuestion.getQuestionNumber()+". "+toeicQuestion.getContent());
+        this.answerSelectionComponent.setCorrectAnswer(toeicQuestion.getCorrectAnswer());
+        this.answerSelectionComponent.setToeicAnswerChoices(choices);
     }
 
     @Override
     public void showExplain() {
+        this.answerSelectionComponent.setShowExplain(!this.answerSelectionComponent.isShowExplain());
+    }
+    @Override
+    public Integer getNumberCorrectAnswer() {
+        final ToeicAnswerChoice currentChoice = this.answerSelectionComponent.getCurrentChoice();
+        if (currentChoice != null) {
+            if (currentChoice.getLabel().equals(this.answerSelectionComponent.getCorrectAnswer())) {
+                return 1;
+            }
+        }
+        return 0;
+    }
 
+    @Override
+    public Integer getTotalQuestions() {
+        return 1;
     }
 
 }
