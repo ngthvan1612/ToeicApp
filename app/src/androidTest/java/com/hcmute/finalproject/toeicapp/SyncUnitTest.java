f452d9031e64dac9b6620c94e800f45aba3f1220
Nguyễn Thành Văn <ngthvan1612@gmail.com>
2023-05-15 18:57:08 +0700

    Update SyncUnitTest.java

--- app/src/androidTest/java/com/hcmute/finalproject/toeicapp/SyncUnitTest.java
+++ app/src/androidTest/java/com/hcmute/finalproject/toeicapp/SyncUnitTest.java
@@ -2,6 +2,7 @@ package com.hcmute.finalproject.toeicapp;
 
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import org.json.JSONObject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,11 +12,18 @@ import android.content.Context;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import com.google.gson.Gson;
 import com.hcmute.finalproject.toeicapp.activities.HomeActivity;
 import com.hcmute.finalproject.toeicapp.database.ToeicAppDatabase;
 import com.hcmute.finalproject.toeicapp.services.backend.tests.ToeicTestBackendService;
+import com.hcmute.finalproject.toeicapp.services.backend.tests.model.AndroidToeicFullTest;
+import com.hcmute.finalproject.toeicapp.services.backend.tests.model.AndroidToeicTestsResponse;
 import com.hcmute.finalproject.toeicapp.services.backend.vocabs.ToeicVocabularyBackendService;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.concurrent.CountDownLatch;
 
 @RunWith(AndroidJUnit4.class)
@@ -50,8 +58,32 @@ public class SyncUnitTest {
 
         signal.await();
 
-        ToeicAppDatabase toeicAppDatabase = ToeicAppDatabase.getInstance(context);
-        assert toeicAppDatabase.getToeicFullTestDao().getAll().size() == 8;
+        String str="https://toeic-app.uteoj.com/api/android/toeic-test/get-tests-data";
+        URLConnection urlConn = null;
+        BufferedReader bufferedReader = null;
+        try
+        {
+            URL url = new URL(str);
+            urlConn = url.openConnection();
+            bufferedReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                stringBuffer.append(line);
+            }
+
+            Gson gson = new Gson();
+            AndroidToeicTestsResponse response = gson.fromJson(stringBuffer.toString(), AndroidToeicTestsResponse.class);
+            ToeicAppDatabase toeicAppDatabase = ToeicAppDatabase.getInstance(context);
+            assert toeicAppDatabase.getToeicFullTestDao().getAll().size() == response.getData().size();
+        }
+        catch(Exception ex)
+        {
+            result[0] = false;
+        }
+
         assert result[0];
     }
 
