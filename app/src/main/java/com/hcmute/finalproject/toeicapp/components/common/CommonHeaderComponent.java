72b63a162fad46750d0cbc86b804f0ec577dd01d
baongoc02 <105281228+baongoc02@users.noreply.github.com>
2023-05-15 12:16:13 +0700

    fix backbutton
     in common header

--- app/src/main/java/com/hcmute/finalproject/toeicapp/components/common/CommonHeaderComponent.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/components/common/CommonHeaderComponent.java
@@ -1,18 +1,22 @@
 package com.hcmute.finalproject.toeicapp.components.common;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import com.hcmute.finalproject.toeicapp.R;
+import com.hcmute.finalproject.toeicapp.activities.HomeActivity;
 
-    public class CommonHeaderComponent extends LinearLayout {
+public class CommonHeaderComponent extends LinearLayout {
     private TextView txtTitle;
+    private BackButtonRoundedComponent btnBack;
     public CommonHeaderComponent(Context context) {
         this(context, null);
     }
@@ -32,13 +36,15 @@ import com.hcmute.finalproject.toeicapp.R;
     private void initComponent(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         View view = inflate(context, R.layout.component_common_header, this);
         this.txtTitle = view.findViewById(R.id.component_common_header_title);
+        btnBack = findViewById(R.id.component_common_header_back_button);
+        btnBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity)context).onBackPressed();
+            }
+        });
     }
     public void setTitle(String title) {
         this.txtTitle.setText(title);
     }
-    public void handleBackToHomepage(Intent intent) {
-        BackButtonRoundedComponent backBtn =  findViewById(R.id.component_common_back_button);
-        backBtn.handleBackToHomepage(intent);
-    }
-
 }
