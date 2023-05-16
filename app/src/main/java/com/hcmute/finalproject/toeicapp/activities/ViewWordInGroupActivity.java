53dd569392bcca729c8aedbf695dcf7471aa6f89
Nguyễn Thành Văn <ngthvan1612@gmail.com>
2023-05-16 20:05:35 +0700

    FEAT: Add FavoriteToeicVocabWord

--- app/src/main/java/com/hcmute/finalproject/toeicapp/activities/ViewWordInGroupActivity.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/activities/ViewWordInGroupActivity.java
@@ -1,8 +1,12 @@
 package com.hcmute.finalproject.toeicapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.hcmute.finalproject.toeicapp.R;
 import com.hcmute.finalproject.toeicapp.components.common.CommonHeaderComponent;
@@ -16,5 +20,38 @@ public class ViewWordInGroupActivity extends AppCompatActivity {
 
         String groupName = getIntent().getStringExtra("groupName");
         ((CommonHeaderComponent)findViewById(R.id.activity_view_word_in_group_header_title)).setTitle(groupName);
+
+        RecyclerView recyclerView = findViewById(R.id.activity_view_word_in_group_rcView);
+
+    }
+
+    private class WordAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        private class WordItemHolder extends RecyclerView.ViewHolder {
+
+            public WordItemHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+
+            public void setData() {
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/dao/FavoriteVocabWordDao.java
@@ -0,0 +1,16 @@
+package com.hcmute.finalproject.toeicapp.dao;
+
+import androidx.room.Query;
+
+import com.hcmute.finalproject.toeicapp.entities.FavoriteVocabGroup;
+import com.hcmute.finalproject.toeicapp.entities.FavoriteVocabWord;
+
+import java.util.List;
+
+public interface FavoriteVocabWordDao extends ToeicDao<FavoriteVocabWord> {
+    @Query("SELECT * FROM FavoriteVocabWord")
+    List<FavoriteVocabWord> getAll();
+
+    @Query("SELECT * FROM FavoriteVocabWord where id = :id")
+    FavoriteVocabWord getOne(Integer id);
+}
