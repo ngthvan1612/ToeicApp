9ad87ef7b8b5cf917cb073194a4709ee2c04fec0
qhuong1008 <qhuong1008@gmail.com>
2023-05-16 14:05:49 +0700

    custom dialog layout

---
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/components/part_one/CustomDialogComponent.java
@@ -0,0 +1,38 @@
+package com.hcmute.finalproject.toeicapp.components.part_one;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.hcmute.finalproject.toeicapp.R;
+
+public class CustomDialogComponent {
+    public void showDialog(Activity activity, String type, String msg){
+        final Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        if(type == "ERROR") {
+            dialog.setContentView(R.layout.component_error_alert_dialog);
+        }
+        else {
+            dialog.setContentView(R.layout.component_success_alert_dialog);
+        }
+
+        TextView text = (TextView) dialog.findViewById(R.id.text_dialog);
+        text.setText(msg);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.btn_dialog);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+    }
+}
--- app/src/main/java/com/hcmute/finalproject/toeicapp/testing/huong/activities/HuongTestActivity.java
+++ app/src/main/java/com/hcmute/finalproject/toeicapp/testing/huong/activities/HuongTestActivity.java
@@ -12,23 +12,10 @@ import com.hcmute.finalproject.toeicapp.activities.HomeActivity;
 import com.hcmute.finalproject.toeicapp.components.LoadingDialogComponent;
 
 public class HuongTestActivity extends AppCompatActivity {
-    private Button btn;
-    Dialog dialog;
 
-    private LoadingDialogComponent loadingDialogComponent = new LoadingDialogComponent(HuongTestActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        dialog = new Dialog(this,android.R.style.Theme_Light);
         setContentView(R.layout.activity_huong_test);
-        btn = (Button) findViewById(R.id.loading_demo_button);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = "Đang đồng bộ dữ liệu";
-                loadingDialogComponent.startLoadingDialog(dialog,message);
-            }
-        });
     }
 }
